b0bdda45c6ea254b3cbb9ff19134607a35f3d2f4
adenning2 <adenning2@byui.edu>
2023-10-10 16:43:29 -0500

    change button message

--- screens/Login.js
+++ screens/Login.js
@@ -5,7 +5,7 @@ export default function Login(props){
     return(
         <View style={styles.login}>
             <Text>This is the Login Screen</Text>
-            <Button title="Log In" onPress={()=>{Alert.alert("Hello _______");}}></Button>
+            <Button title="Log In" onPress={()=>{Alert.alert("Hello from Adam!");}}></Button>
             <Button title="Log In #2" onPress={()=>{Alert.alert("Hello from Nathan!");}}></Button>
         </View>
 
